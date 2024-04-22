470a0ff4d37c709b966bc8aa7e69bdceb524725c
azur <azur99@web.d>
2024-04-22 18:02:48 +0200

    Full Videos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import VideoContainer from './components/VideoContainer.svelte';
+	let videos = ['BossbabyIntro.webm', 'Bundeswehr.webm', 'Chrysanthemen.webm', 'DeineFreundinnen.webm', 'Diamantstein.webm', 'FuerImmer.webm', 'HartGehenOhneGrund.webm', 'Herbst.webm', 'IchWuenschteEsWuerdMichKuemmern.webm', 'LieferandoFuer2.webm', 'LoserABC.webm', 'NowitzkiFlow.webm', 'Shotta2.webm', 'SterneNehmen.webm', 'TheorieUndPraxis.webm', 'Woah.webm'];
 </script>
 
 <div class="grid grid-cols-4 grid-rows-4 bg-white w-screen h-screen">
-	<VideoContainer video={'Diamantstein.webm'} />
-	<VideoContainer video={'Lieferando.webm'} />
-	<VideoContainer video={'Chrysanthemen.webm'} />
+	{#each videos as video}
+		<VideoContainer video={video} />
+	{/each}
 </div>
--- src/routes/components/VideoContainer.svelte
+++ src/routes/components/VideoContainer.svelte
@@ -3,19 +3,33 @@
 	export let video: string;
 	let videoSrc = `src/public/${video}`;
 	let isHovered = false;
+	let videoElement: HTMLVideoElement;
+
+	function handleMouseEnter() {
+		isHovered = true;
+		if (!videoElement.paused && videoElement.readyState > 2) {
+			videoElement.muted = false; // Unmute the video
+			videoElement.play().catch(error => console.error("Failed to play video:", error));
+		}
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
 </script>
 
 <div class="flex justify-center items-center">
-	<div class="relative overflow-hidden size-full transition-transform transform hover:scale-125">
+	<div class="relative overflow-hidden size-full transition-transform transform hover:scale-125"
+		on:mouseenter={handleMouseEnter}
+		on:mouseleave={handleMouseLeave}>
 		<video
 			id="videoPlayer"
 			disablePictureInPicture
 			autoplay
-			muted
 			loop
 			class="w-full h-full object-cover"
-			on:mouseenter={() => (isHovered = true)}
-			on:mouseleave={() => (isHovered = false)}
+			muted
+			bind:this={videoElement}
 			style={`z-index: ${isHovered ? 1 : 0};`}
 		>
 			<source src={videoSrc} type="video/webm" />
