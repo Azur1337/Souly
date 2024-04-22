52b3e65c7a746d4244db7169560c6f5415b4c088
Azur1337 <azur99@web.de>
2024-04-22 14:55:32 +0200

    WIP

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import VideoContainer from './components/VideoContainer.svelte';
+</script>
+
+<div class="grid grid-cols-4 grid-rows-4 bg-white w-screen h-screen">
+	<VideoContainer video={'Diamantstein.webm'} />
+	<VideoContainer video={'Lieferando.webm'} />
+	<VideoContainer video={'Chrysanthemen.webm'} />
+</div>
---
+++ src/routes/components/VideoContainer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	export let video: string;
+	let videoSrc = `src/public/${video}`;
+	let isHovered = false;
+</script>
+
+<div class="flex justify-center items-center">
+	<div class="relative overflow-hidden size-full transition-transform transform hover:scale-125">
+		<video
+			id="videoPlayer"
+			disablePictureInPicture
+			autoplay
+			muted
+			loop
+			class="w-full h-full object-cover"
+			on:mouseenter={() => (isHovered = true)}
+			on:mouseleave={() => (isHovered = false)}
+			style={`z-index: ${isHovered ? 1 : 0};`}
+		>
+			<source src={videoSrc} type="video/webm" />
+			Your browser does not support the video tag.
+		</video>
+	</div>
+</div>
